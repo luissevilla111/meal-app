9d04004091a2542f3432e463daeb981bde856edb
luissevilla <luissevilla111@gmail.com>
2023-07-29 08:35:10 -0600

    adding more example meals

--- data/dummydata.ts
+++ data/dummydata.ts
@@ -67,7 +67,7 @@ export const MEALS_DUMMY: IMeal[] = [
     categories: ["C1", "C4"],
     duration: "60",
     image:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+      "https://img.freepik.com/fotos-premium/mole-poblano-tradicional-arroz_123827-12896.jpg",
     ingredients: [
       { title: "Chile Poblano", icon: "", quntity: 4, mesure: "Un" },
     ],
@@ -82,8 +82,7 @@ export const MEALS_DUMMY: IMeal[] = [
     benefits: [],
     categories: ["C1", "C3"],
     duration: "10",
-    image:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+    image: "https://assets.stickpng.com/thumbs/58727facf3a71010b5e8ef07.png",
     ingredients: [
       { title: "Tortilla", icon: "", quntity: 5, mesure: "Un" },
       { title: "Carne de res", icon: "", quntity: 1, mesure: "Kg" },
@@ -100,14 +99,13 @@ export const MEALS_DUMMY: IMeal[] = [
     categories: ["C5"],
     duration: "30",
     image:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+      "https://w7.pngwing.com/pngs/388/761/png-transparent-fast-food-mcdonald-s-1-store-museum-mozzarella-sticks-marinara-sauce-cheese-fast-food-mcdonald-s-store-thumbnail.png",
     ingredients: [
       { title: "queso", icon: "", quntity: 0.5, mesure: "Kg" },
       { title: "pierna de pollo", icon: "", quntity: 2, mesure: "Un" },
     ],
     steps: "YYYYYYYYYYYYYYYYYYYYYYYY",
-    video:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+    video: "",
   },
   {
     id: "M4",
@@ -117,15 +115,14 @@ export const MEALS_DUMMY: IMeal[] = [
     categories: ["C2", "C3"],
     duration: "40",
     image:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/cajaregistradora-776cc.appspot.com/o/pngwing.com(2).png?alt=media&token=bde13cf8-4b17-4da9-90cf-c16db83b2e1f",
     ingredients: [
       { title: "queso", icon: "", quntity: 3, mesure: "Un" },
       { title: "carne molida", icon: "", quntity: 0.2, mesure: "Gr" },
       { title: "Pan", icon: "", quntity: 2, mesure: "Un" },
     ],
     steps: "YYYYYYYYYYYYYYYYYYYYYYYY",
-    video:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+    video: "",
   },
   {
     id: "M5",
@@ -134,8 +131,7 @@ export const MEALS_DUMMY: IMeal[] = [
     benefits: [],
     categories: ["C2", "C3"],
     duration: "40",
-    image:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+    image: "https://assets.stickpng.com/images/580b57fcd9996e24bc43c1b7.png",
     ingredients: [
       { title: "salchichas", icon: "", quntity: 2, mesure: "Un" },
       { title: "Pan", icon: "", quntity: 1, mesure: "Un" },
@@ -153,15 +149,14 @@ export const MEALS_DUMMY: IMeal[] = [
     categories: ["C9"],
     duration: "200",
     image:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+      "https://www.pngkit.com/png/full/598-5986228_tres-leches-decoracion-pastel-3-leches.png",
     ingredients: [
       { title: "Huevo", icon: "", quntity: 2, mesure: "Kg" },
       { title: "leche", icon: "", quntity: 3, mesure: "Lts" },
       { title: "Fresas", icon: "", quntity: 5, mesure: "Un" },
     ],
     steps: "fsodifsdf.....",
-    video:
-      "https://www.eluniversal.com.mx/resizer/P2WeOYmFsEeBn-puypa2Wdplqgs=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/M7BQKUZB45F5RF4D6YY2F2XDSY.jpg",
+    video: "",
   },
 
   {
